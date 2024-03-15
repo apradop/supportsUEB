8092c4daf5f55e5b6dd86543754593c14dfcb49d
apradop <61154699+apradop@users.noreply.github.com>
2024-03-14 22:10:39 -0500

    Se corrije pagina del server
    Se genera el cambio del admin

--- app/listadoClasesFin/page.jsx
+++ app/listadoClasesFin/page.jsx
@@ -1,7 +1,7 @@
-"use client";
+
 import ListadoClasesFin from "@/components/ListadoClaseFin"
 import Navigation from "@/components/Navigation";
-import useExcel from "@/hooks/useExcel";
+
 
 
 async function consulta() {
@@ -20,35 +20,6 @@ async function consulta() {
   console.log(dat);
   return (dat);
 }
-
-async function exportToExcel() { 
-
-  const clases = await consulta();
-
-  var arreglo = clases.map((clase) => {
-
-    return {
-      RESPONSABLE: clase.responsable,
-      PROGRAMA: clase.programa,
-      MATERIA: clase.materia,
-      SALON: clase.salon,
-      FECHAS: clase.fecha,
-      "HORA INICIAL": clase.horai,
-      "HORA FINAL": clase.horaf,
-      "HORA INICIAL REAL": clase.horaIniReal,
-      "HORA FINAL REAL": clase.horaFinalReal,
-      PROGRAMAS: clase.programas.values.toString(),
-      OBSERVACIONES: clase.observaciones,
-    };
-    });
-
-
-  const { useExportToExcel } = useExcel();
-
-  useExportToExcel(arreglo, "ClasesFinalizadasReporte");
-
-
-}
  
 async function page(){
 
@@ -58,31 +29,7 @@ async function page(){
     return (
         <>
         <Navigation />
-        <div className="container">
-        <h1>
-            Listado de Clases Finalizadas
-        </h1>
-
-        <button type="button" className="btn btn-primary" onClick={() => exportToExcel()}>Descargar Reporte</button>
-
-
-        <table className="table table-hover">
-  <thead>
-    <tr>
-    <th scope="col">N°</th>
-      <th scope="col">Fecha</th>
-      <th scope="col">Horario Inicial</th>
-      <th scope="col">Horario Final</th>
-      <th scope="col">Salón</th>
-      <th scope="col">Responsable</th>
-      <th scope="col">Materia</th>
-    </tr>
-    </thead>
-    <tbody>
-      <ListadoClasesFin clases = {clases}/>
-  </tbody>
-</table>
-</div>
+        <ListadoClasesFin clases = {clases}/>
 </>
     )
 
--- components/ListadoClaseFin.jsx
+++ components/ListadoClaseFin.jsx
@@ -1,15 +1,61 @@
 "use client";
-
+import useExcel from "@/hooks/useExcel";
 
 function listadoClasesFin ({clases}) {
 
-    //console.log(hoy.toLocaleDateString());
+    function exportToExcel(clas) { 
+
+        var arreglo = clas.map((clase) => {
+      
+          return {
+            RESPONSABLE: clase.responsable,
+            PROGRAMA: clase.programa,
+            MATERIA: clase.materia,
+            SALON: clase.salon,
+            FECHAS: clase.fecha,
+            "HORA INICIAL": clase.horai,
+            "HORA FINAL": clase.horaf,
+            "HORA INICIAL REAL": clase.horaIniReal,
+            "HORA FINAL REAL": clase.horaFinalReal,
+            PROGRAMAS: clase.programas.values.toString(),
+            OBSERVACIONES: clase.observaciones,
+          };
+          });
+      
+      
+        const { useExportToExcel } = useExcel();
+      
+        useExportToExcel(arreglo, "ClasesFinalizadasReporte");
+      
+      
+      }
 
 
 
     return (
         <>
-        {clases.map((clase,index) => (
+        <div className="container">
+        <h1>
+            Listado de Clases Finalizadas
+        </h1>
+
+        <button type="button" className="btn btn-primary" onClick={() => exportToExcel(clases)}>Descargar Reporte</button>
+
+
+        <table className="table table-hover">
+  <thead>
+    <tr>
+    <th scope="col">N°</th>
+      <th scope="col">Fecha</th>
+      <th scope="col">Horario Inicial</th>
+      <th scope="col">Horario Final</th>
+      <th scope="col">Salón</th>
+      <th scope="col">Responsable</th>
+      <th scope="col">Materia</th>
+    </tr>
+    </thead>
+    <tbody>
+    {clases.map((clase,index) => (
             
             <tr key={clase.id}>
             <th scope="row">{index+1}</th>
@@ -21,6 +67,10 @@ function listadoClasesFin ({clases}) {
             <td >{clase.materia}</td>
             </tr>
         ))}
+  </tbody>
+</table>
+</div>
+
         </>
         
     )
