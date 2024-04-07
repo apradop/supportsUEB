5d73829c3b2d9260e5bf6c2ae9612b6927d07dbd
apradop <61154699+apradop@users.noreply.github.com>
2024-04-06 22:49:33 -0500

    Se justan los reportes
    + Se crea metodo para mostrar las columnas de los programas que se utilizaron
    - Se elimina codigo que no servia

--- components/ListadoClaseFin.jsx
+++ components/ListadoClaseFin.jsx
@@ -2,9 +2,63 @@
 import useExcel from "@/hooks/useExcel";
 
 function listadoClasesFin({ clases }) {
+  function contarRepetidos(arr) {
+   
+    var conteo = {};
+
+    arr.forEach(function (elemento) {
+      if (conteo[elemento]) {
+        conteo[elemento]++;
+      } else {
+        conteo[elemento] = 1;
+      }
+    });
+    var conteoArray = [];
+    for (var clave in conteo) {
+      conteoArray.push({ elemento: clave, repeticiones: conteo[clave] });
+    }
+
+    conteoArray.sort(function (a, b) {
+      return b.repeticiones - a.repeticiones;
+    });
+    return conteoArray;
+  }
+
   function exportToExcel(clas) {
+    var programas = clas.map((clase) => {
+      var primerArreglo = {
+        PROGRAMAS: clase.programas.values.toString(),
+      };
+
+      return primerArreglo;
+    });
+
+    var prog1 = programas.map((programa) => {
+      return programa.PROGRAMAS.split(",");
+    });
+
+    const nuevoProgramas = [].concat(...prog1);
+
+    var repetidos = contarRepetidos(nuevoProgramas);
+
     var arreglo = clas.map((clase) => {
-      return {
+
+      var a = {};
+
+      var b = clase.programas.values.toString().split(",");
+
+      for (var i = 0; i < repetidos.length; i++) {
+        for (var j = 0; j < b.length; j++) {
+          if (repetidos[i].elemento === b[j]) {
+            a = {
+              ...a,
+              [repetidos[i].elemento]: "X"
+            };
+          }
+        }
+      }
+
+      var primerArreglo = {
         RESPONSABLE: clase.responsable,
         PROGRAMA: clase.programa,
         MATERIA: clase.materia,
@@ -14,11 +68,15 @@ function listadoClasesFin({ clases }) {
         "HORA FINAL": clase.horaf,
         "HORA REGISTRO": clase.horaIniReal,
         "HORA FINAL REGISTRO": clase.horaFinalReal,
-        DURACION : clase.duracion,
-        PROGRAMAS: clase.programas.values.toString(),
+        DURACION: clase.duracion,
         OBSERVACIONES: clase.observaciones,
+        ...a
       };
+      
+      return primerArreglo;
     });
+
+    console.log(arreglo);
 
     const { useExportToExcel } = useExcel();
 
@@ -39,38 +97,55 @@ function listadoClasesFin({ clases }) {
         </button>
 
         <div className="table-responsive">
-        <table className="table table-hover">
-          <thead>
-            <tr>
-              <th scope="col">N°</th>
-              <th scope="col">Fecha</th>
-              <th scope="col">Horario Inicial</th>
-              <th scope="col">Horario Final</th>
-              <th scope="col">Salón</th>
-              <th scope="col">Responsable</th>
-              <th scope="col">Materia</th>
-            </tr>
-          </thead>
-          <tbody>
-            {clases.map((clase, index) => (
-              <tr key={clase.id}>
-                <th scope="row">{index + 1}</th>
-                <th scope="row">{clase.fecha}</th>
-                <td> {clase.horai}</td>
-                <td> {clase.horaf}</td>
-                <td>{clase.salon}</td>
-                <td>{clase.responsable}</td>
-                <td>{clase.materia}</td>
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">N°</th>
+                <th scope="col">Fecha</th>
+                <th scope="col">Horario Inicial</th>
+                <th scope="col">Horario Final</th>
+                <th scope="col">Salón</th>
+                <th scope="col">Responsable</th>
+                <th scope="col">Materia</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {clases.map((clase, index) => (
+                <tr key={clase.id}>
+                  <th scope="row">{index + 1}</th>
+                  <th scope="row">{clase.fecha}</th>
+                  <td> {clase.horai}</td>
+                  <td> {clase.horaf}</td>
+                  <td>{clase.salon}</td>
+                  <td>{clase.responsable}</td>
+                  <td>{clase.materia}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-
-        
       </div>
     </>
   );
 }
 
 export default listadoClasesFin;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
