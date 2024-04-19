47d4bab83d30b1a99fb5f4553eb8301a4a37acf6
apradop <61154699+apradop@users.noreply.github.com>
2024-04-18 22:51:33 -0500

    se actualizar la db
    se actuliza el archivo de la db para que sea dependiente a los datos

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mariadb = require('mariadb');
 export const pool = mariadb.createPool({
-     host: 'localhost',
-     user:'root', 
-     password: 'admin',
-     database: 'supportueb',
-     connectionLimit: 5
+     host: process.env.DB_HOST,
+     user: process.env.DB_USER , 
+     password: process.env.DB_PASS ,
+     database: process.env.DB_DATABASE ,
+     connectionLimit: process.env.DB_CONNECTION 
 });
